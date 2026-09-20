1e065f3cc538a48eaa89d1de61fe4e214c30d35e
cosscoll <pro.collomb@gmail.com>
2026-09-21 01:22:50 +0200

    Delete backend directory

--- backend/src/routes/ue.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { listUEs, getUE } from '../controllers/ue.controller.js';
-
-const router = Router();
-
-router.get('/', listUEs);
-router.get('/:code', getUE);
-
-export default router;
